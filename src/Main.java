e3e4d2c2b5cb20661ebf3a04babde7f2129faccc
RickaPrincy <rckprincy@gmail.com>
2023-12-03 16:08:10 +0300

    feat: create database connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import repository.PostgresqlConnection;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Connection connection = PostgresqlConnection.getConnection();
     }
 }
