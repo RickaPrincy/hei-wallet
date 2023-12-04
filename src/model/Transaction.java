b1c08a8aa2afa94d30461e364d37c3d4d0a2114d
sanda03 <hei.sanda.2@gmail.com>
2023-12-05 01:45:48 +0300

    feat: transaction implementation

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaction {
-    private String id, ref, description;
-    private Double amount;
+    private String id, description;
+    private BigDecimal amount;
     private Account sourceAccount, destinationAccount;
     private Timestamp transactionDatetime;
+    private TransactionType type;
 }
