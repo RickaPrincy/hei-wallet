6e5294d6d0ec1a28e1b55955d32bc98ce5b6a405
sanda03 <hei.sanda.2@gmail.com>
2023-12-05 01:11:48 +0300

    feat: account repository

--- src/model/Account.java
+++ src/model/Account.java
@@ -3,10 +3,18 @@ package model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import repository.CurrencyRepository;
+
+import java.math.BigDecimal;
+import java.sql.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account {
+    private String id, name, comment, owner, bankName, number;
+    private Date creation_date;
+    private BigDecimal balance;
     private AccountType type;
+    private Currency currency;
 }
