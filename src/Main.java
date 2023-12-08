5a8a3aeed1b235150d5809cf184b3e0f617ae786
RickaPrincy <rckprincy@gmail.com>
2023-12-08 13:11:47 +0300

    chore: put example in main

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,14 @@
-import model.*;
-import repository.AccountRepository;
-import repository.CurrencyRepository;
-import repository.Pair;
-import repository.TransactionRepository;
-
-import java.math.BigDecimal;
-import java.sql.Date;
+import mock.tests.AccountTest;
+import mock.tests.BalanceTest;
+import mock.tests.CurrencyTest;
+import mock.tests.TransactionTest;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        AccountRepository accountRepository = new AccountRepository();
-        CurrencyRepository currencyRepository = new CurrencyRepository();
-        TransactionRepository transactionRepository= new TransactionRepository();
-        Currency insertedCurrency = new Currency("currency_dollar", "name_inserted", "code_inserted", null, 100, Date.valueOf("2023-01-01"));
-
-        //filter by id test
-        currencyRepository
-                .findAll(Map.of("id", new Pair("currency_ariary",true)))
-                .forEach(System.out::println);
-
-        //saveAll and save (save or update)
-        currencyRepository.saveAll(List.of(
-            insertedCurrency,
-            new Currency("currency_ariary", "name_inserted", "code_inserted", null, 100, Date.valueOf("2023-01-01"))
-        ));
-
-        //findALlWith no filter
-        currencyRepository.findAll(null).forEach(System.out::println);
-
-        //insert account
-        Account insertAccount = new Account(
-            "account_id1",
-            "nametest",
-            "test",
-            "test",
-            "test",
-            "tttt5t",
-            Date.valueOf("2023-01-01"),
-            BigDecimal.valueOf(5),
-            AccountType.BUSINESS,
-            insertedCurrency
-        );
-        System.out.println(accountRepository.save(insertAccount));
-
-        //findAllAccount
-        accountRepository.findAll(null).forEach(System.out::println);
-
-        //create transaction
-        System.out.println(transactionRepository.saveAll(List.of(
-                new Transaction(null, "test", BigDecimal.valueOf(555), insertAccount, null, Timestamp.valueOf("2023-01-01 01:01:01"), TransactionType.DEPOSIT)
-        )));
-
-        //findAllAccount
-        transactionRepository.findAll(null).forEach(System.out::println);
+        CurrencyTest.findAll();
+        BalanceTest.findAll();
+        TransactionTest.findAll();
+        AccountTest.findAll();
     }
 }
