78938110a2cfa2602e967e40595f2ba62ecf8e62
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2023-12-08 08:04:01 +0300

    migration: change psql scripts
    * chore: currency to currencies
    
    * docs: change request body of create account
    
    * migration: refactor sql scripts
    
    ---------
    
    Co-authored-by: sanda03 <hei.sanda.2@gmail.com>

--- resources/create_account_type.sql
+++ resources/create_account_type.sql
@@ -1,5 +1,5 @@
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'account_type') THEN
-        CREATE TYPE "account_type" AS ENUM ('CHECKING', 'SAVING', 'BUSINESS', 'INVESTMENT', 'CERTIFICATE_OF_DEPOSIT');
+        CREATE TYPE "account_type" AS ENUM ('CASH', 'MOBILE_MONEY', 'BANK');
     END IF;
 END $$;
--- resources/create_transaction_type.sql
+++ resources/create_transaction_type.sql
@@ -1,5 +1,5 @@
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
-        CREATE TYPE "transaction_type" AS ENUM ('DEPOSIT', 'WITHDRAWAL', 'TRANSFER');
+        CREATE TYPE "transaction_type" AS ENUM ( 'CREDIT', 'DEBIT' );
     END IF;
 END $$
