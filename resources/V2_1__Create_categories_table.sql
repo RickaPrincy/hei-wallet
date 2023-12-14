45da98d98ea1e9d49732379dfaf63ec1027b41a6
sanda03 <hei.sanda.2@gmail.com>
2023-12-14 10:30:30 +0100

    migration: create table category

---
+++ resources/V2_1__Create_categories_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS "category" (
+    "id" VARCHAR(255) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(225) UNIQUE NOT NULL
+);
+
+INSERT INTO "category"
+SELECT 'category_1', 'Restaurant'
+WHERE NOT EXISTS (
+    SELECT 1 FROM "category"
+    WHERE
+        "id"='category_1' AND "name"='Restaurant'
+);
+INSERT INTO "category"
+SELECT 'category_2', 'Téléphone'
+WHERE NOT EXISTS (
+    SELECT 1 FROM "category"
+    WHERE
+            "id"='category_2' AND "name"='Téléphone'
+);
+
+INSERT INTO "category"
+SELECT 'category_3', 'Multimedia'
+WHERE NOT EXISTS (
+    SELECT 1 FROM "category"
+    WHERE
+            "id"='category_3' AND "name"='Multimedia'
+);
