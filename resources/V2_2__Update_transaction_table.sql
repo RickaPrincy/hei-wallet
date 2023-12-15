2f9b573882415c8497de222e0c942a88562c6fbf
sanda03 <hei.sanda.2@gmail.com>
2023-12-15 09:56:06 +0300

    fix: fix sum_in_out_by_category

--- resources/V2_2__Update_transaction_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "transaction"
-ADD COLUMN IF NOT EXISTS "transaction_date" DATETIME NOT NULL,
-ADD COLUMN IF NOT EXISTS "category" VARCHAR(255) DEFAULT 'category_1' NOT NULL,
-ADD CONSTRAINT fk_transaction_category FOREIGN KEY ("category") REFERENCES "category"("id");
-
