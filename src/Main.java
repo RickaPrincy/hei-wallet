85165c465a85c88574cbcf12bfcaaf2fef170a25
RickaPrincy <rckprincy@gmail.com>
2024-01-29 22:28:43 +0300

    feat: save and saveAll implemented

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import model.Category;
+import model.CategoryType;
 import repository.CategoryCrudOperations;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        final Category category = new Category(
+            "category_1",
+            "name_updated" ,
+            CategoryType.ALL
+        );
+
         CategoryCrudOperations categoryCrudOperations = new CategoryCrudOperations();
-        System.out.println(categoryCrudOperations.findOne("category_2"));
+        categoryCrudOperations.findAll().forEach(System.out::println);
+        System.out.println(categoryCrudOperations.saveAll(List.of(category)));
     }
 }
