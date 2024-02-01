6177da6d09f3bef1573d58e0fe977c8cb18b41df
RickaPrincy <rckprincy@gmail.com>
2024-02-01 07:05:01 +0300

    chore: init project

---
+++ src/test/java/com/hei/wallet/heiwallet/HeiWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.wallet.heiwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
